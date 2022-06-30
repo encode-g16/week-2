6800221fa5d64d0e90613d725ca03999e330de08
ben <phas0ruk@gmail.com>
2022-06-30 15:58:26 -0700

    fixes tsconfig

--- Project/contracts/Token.sol
+++ Project/contracts/Token.sol
@@ -20,10 +20,11 @@ contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
 
     // The following functions are overrides required by Solidity.
 
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
         super._afterTokenTransfer(from, to, amount);
     }
 
